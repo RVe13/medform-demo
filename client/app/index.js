2d69810d4af0fa7e8d20c7753ff556fd553c7af0
RVe13 <mimo.massim@gmail.com>
2024-04-27 15:37:49 +0100

    added some files and entries
    nthn important

---
+++ client/app/index.js
@@ -0,0 +1,15 @@
+import {View, Text, SafeAreaView} from "react-native";
+import {useState} from "react" 
+import {useRouter, Stack} from "expo-router"
+
+const Home = ()=>{
+    return (
+        <SafeAreaView style={{flex: 1}}>
+            <Text>
+                Home
+            </Text>
+        </SafeAreaView>
+    )
+}
+
+export default Home;
