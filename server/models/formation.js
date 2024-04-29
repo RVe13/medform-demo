d9ab53300b191b98ebd75d1f037f2fe452f82ad5
RVe13 <mimo.massim@gmail.com>
2024-04-29 15:44:13 +0100

    added some server functionality
    didn't try em cuz mongodb don't wanna connect to my neovim so imma restart and boot to windows vscode and try

---
+++ server/models/formation.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const formationSchema = new Schema({
+    title:{
+        type: String, require: true,
+    },
+    content:{
+        type: String, require: true,
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    },
+    image:{
+        data: Buffer,
+        contentType: String,
+    }
+}, {timestamps: true})
+
+const Formation = mongoose.model("Formation", formationSchema)
+
+module.exports = Formation
---
+++ server/models/medicaments.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const medicamentSchema = new Schema({
+    title:{
+        type: String, require: true,
+    },
+    content:{
+        type: String, require: true,
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    },
+    image:{
+        data: Buffer,
+        contentType: String,
+    }
+}, {timestamps: true})
+
+const Medicament = mongoose.model("Formation", medicamentSchema)
+
+module.exports = Medicament;
